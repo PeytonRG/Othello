bb312dc768b9cea018ebc2f45c85c577a1a6d7a9
Peyton Gasink <peytongasink@att.net>
2020-03-11 17:06:28 -0500

    Green Light |

--- othello/create.py
+++ othello/create.py
@@ -2,6 +2,15 @@
     Created on March 11, 2020
     @author: Peyton Gasink
 '''
-def _create(parms):
-    result = {'create': 'create stub'}
+def _create(inputDictionary):
+    light = inputDictionary["light"]
+    dark = inputDictionary["dark"]
+    blank = inputDictionary["blank"]
+    boardSize = inputDictionary["size"]
+    
+    board = []
+    for index in range(boardSize^2):
+        board.append(blank)
+        
+    result = {"board": board}
     return result

580cd210d3e2f55dd1d14412a090129be72a3382
Peyton Gasink <peytongasink@att.net>
2020-03-11 15:26:44 -0500

    Red Light |

--- othello/test/createTest.py
+++ othello/test/createTest.py
@@ -5,9 +5,19 @@ import json
 class CreateTest(TestCase):
     
     def setUp(self):
-        TestCase.setUp(self)
+        self.inputDictionary = {}
         
     def tearDown(self):
-        TestCase.tearDown(self)
-        
+        self.inputDictionary = {}
     
+    def setLight(self, light):
+        self.inputDictionary["light"] = light
+        
+    def setDark(self, dark):
+        self.inputDictionary["dark"] = dark
+        
+    def setBlank(self, blank):
+        self.inputDictionary["blank"] = blank
+        
+    def setSize(self, size):
+        self.inputDictionary["size"] = size

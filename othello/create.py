f3d23509950f6bf7d5e4f96ecf86bfb0db11e70d
Peyton Gasink <peytongasink@att.net>
2020-03-11 18:45:51 -0500

    Refactor |

--- othello/create.py
+++ othello/create.py
@@ -7,15 +7,21 @@ def _create(inputDictionary):
     dark = inputDictionary["dark"]
     blank = inputDictionary["blank"]
     lengthWidth = inputDictionary["size"]
+    
     boardSize = lengthWidth**2
     boardMidpoint = boardSize / 2 - 1
     distanceFromMidpoint = lengthWidth / 2
     
     board = []
     for index in range(boardSize):
+        # the token indexed either n below or n + 1 above the midpoint
+        # should be a light token, where n is distanceFromMidpoint, or half
+        # the length/width of the board
         if ((boardMidpoint - distanceFromMidpoint) == index or 
             (boardMidpoint + distanceFromMidpoint + 1) == index):
             board.append(light)
+        # likewise, the token indexed at n + 1 below the midpoint
+        # or n above the midpoint should be a dark token
         elif ((boardMidpoint - distanceFromMidpoint + 1) == index or 
             (boardMidpoint + distanceFromMidpoint) == index):
             board.append(dark)

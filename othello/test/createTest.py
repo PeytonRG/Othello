703aa0bda368404b32b366c8c477a52dd6040a83
Peyton Gasink <peytongasink@att.net>
2020-03-11 15:17:30 -0500

    added createTest.py

---
+++ othello/test/createTest.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+import othello.create as create
+import json
+
+class CreateTest(TestCase):
+    
+    def setUp(self):
+        TestCase.setUp(self)
+        
+    def tearDown(self):
+        TestCase.tearDown(self)
+        
+    

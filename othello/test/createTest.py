d221304bb16b4044e38ef0d6dc7f0c8ddc0449c1
Peyton Gasink <peytongasink@att.net>
2020-03-11 21:50:31 -0500

    Refactor |

--- othello/test/createTest.py
+++ othello/test/createTest.py
@@ -95,7 +95,28 @@ class CreateTest(TestCase):
         self.setDark(5)
         self.setBlank(1)
         self.setSize(10)
-        expectedResult = {'board': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 5, 1, 1, 1, 1, 1, 1, 1, 1, 5, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 'tokens': {'light': 6, 'dark': 5, 'blank': 1}, 'status': 'ok', 'integrity': 'd0f18c5b412ab1dbf89da19baa33cc35f4a7dd0619ce7b7dcb2381d2cb14a412'}
+        expectedResult = {'board': [1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1, 6, 5, 1, 1, 1, 
+                                    1, 1, 1, 1, 1, 5, 6, 
+                                    1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1, 1, 1, 1, 1, 1, 
+                                    1, 1], 'tokens': {
+                                        'light': 6, 
+                                        'dark': 5, 
+                                        'blank': 1
+                                        }, 
+                                    'status': 'ok', 
+                                    'integrity': 'd0f18c5b412ab1dbf89da19ba' + 
+                                    'a33cc35f4a7dd0619ce7b7dcb2381d2cb14a412'}
         result = create._create(self.inputDictionary)
         self.assertEqual(result, expectedResult)
         

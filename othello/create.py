253af9ae39a8d9d70e9270c6256ef451dd3f6a43
Peyton Gasink <peytongasink@att.net>
2020-03-12 22:51:57 -0500

    Refactor |

--- othello/create.py
+++ othello/create.py
@@ -14,44 +14,11 @@ def _create(inputDictionary):
     
     light = _validateLight(inputDictionary, errorList, light)
     
-    try:
-        dark = inputDictionary["dark"]
-        if not isinstance(dark, int):
-            raise TypeError
-        if dark > 9 or dark < 0:
-            errorList.append("The value for dark tokens must " 
-            + "be an integer in the range [0, 9].")
-    except KeyError:
-        dark = 2
-    except TypeError:
-        errorList.append("The value for dark tokens must " 
-            + "be an integer in the range [0, 9].")
+    dark = _validateDark(inputDictionary, errorList, dark)
     
-    try:
-        blank = inputDictionary["blank"]
-        if not isinstance(blank, int):
-            raise TypeError
-        if blank > 9 or blank < 0:
-            errorList.append("The value for blank spaces must " 
-            + "be an integer in the range [0, 9].")
-    except KeyError:
-        blank = 0
-    except TypeError:
-        errorList.append("The value for blank spaces must " 
-            + "be an integer in the range [0, 9].")
+    blank = _validateBlank(inputDictionary, errorList, blank)
         
-    try:
-        lengthWidth = inputDictionary["size"]
-        if not isinstance(lengthWidth, int):
-            raise TypeError
-        if (lengthWidth % 2 != 0) or (lengthWidth > 16) or (lengthWidth < 6):
-            errorList.append("The value for board size must " 
-            + "be an even integer in the range [6, 16].")
-    except KeyError:
-        lengthWidth = 8
-    except TypeError:
-        errorList.append("The value for board size must " 
-            + "be an even integer in the range [6, 16].")
+    lengthWidth = _validateSize(inputDictionary, errorList, lengthWidth)
            
     if light == dark or light == blank or dark == blank:
         errorList.append("The values of light, dark, " 
@@ -112,5 +79,50 @@ def _validateLight(inputDictionary, errorList, light):
         errorList.append(
             "The value for light tokens must " + "be an integer in the range [0, 9].")
     return light
+
+def _validateDark(inputDictionary, errorList, dark):
+    try:
+        dark = inputDictionary["dark"]
+        if not isinstance(dark, int):
+            raise TypeError
+        if dark > 9 or dark < 0:
+            errorList.append("The value for dark tokens must " + "be an integer in the range [0, 9].")
+    except KeyError:
+        dark = 2
+    except TypeError:
+        errorList.append(
+            "The value for dark tokens must " + "be an integer in the range [0, 9].")
+    return dark
+
+def _validateBlank(inputDictionary, errorList, blank):
+    try:
+        blank = inputDictionary["blank"]
+        if not isinstance(blank, int):
+            raise TypeError
+        if blank > 9 or blank < 0:
+            errorList.append("The value for blank spaces must " + "be an integer in the range [0, 9].")
+    except KeyError:
+        blank = 0
+    except TypeError:
+        errorList.append(
+            "The value for blank spaces must " + "be an integer in the range [0, 9].")
+    return blank
+
+def _validateSize(inputDictionary, errorList, lengthWidth):
+    try:
+        lengthWidth = inputDictionary["size"]
+        if not isinstance(lengthWidth, int):
+            raise TypeError
+        if (lengthWidth % 2 != 0) or (lengthWidth > 16) or (lengthWidth < 6):
+            errorList.append("The value for board size must " + "be an even integer in the range [6, 16].")
+    except KeyError:
+        lengthWidth = 8
+    except TypeError:
+        errorList.append(
+            "The value for board size must " + "be an even integer in the range [6, 16].")
+    return lengthWidth
+
+
+
 
 

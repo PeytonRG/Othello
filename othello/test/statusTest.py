c3f9c61fc15aa836241e0fe423a6e764468ef3bc
Peyton Gasink <peytongasink@att.net>
2020-03-28 16:28:50 -0500

    Red Light |

--- othello/test/statusTest.py
+++ othello/test/statusTest.py
@@ -14,3 +14,48 @@ class CreateTest(TestCase):
         
     def tearDown(self):
         TestCase.tearDown(self)
+        
+    def setLight(self, light = None):
+        self.inputDictionary["light"] = light
+        
+    def setDark(self, dark = None):
+        self.inputDictionary["dark"] = dark
+        
+    def setBlank(self, blank = None):
+        self.inputDictionary["blank"] = blank
+        
+    def setBoard(self, board = None):
+        self.inputDictionary["board"] = board
+        
+    def setIntegrity(self, integrity = None):
+        self.inputDictionary["integrity"] = integrity
+        
+#    Desired level of confidence:    boundary value analysis
+#    Input-output Analysis
+#        inputs:        light -> integer, .GE.0.LE9, optional, unvalidated,
+#                                defaults to 1
+#                       dark -> integer, .GE.0.LE9, optional, unvalidated
+#                                defaults to 2
+#                       blank -> integer, .GE.0.LE9, optional, unvalidated
+#                                defaults to 0
+#                       board -> list of integers with length n x n,
+#                                where n is an integer .GE6.LE16, mandatory, unvalidated
+#                       integrity -> 64-character sha256 hash hexdigest, mandatory, unvalidated
+#        outputs:    dictionary -> key: status,
+#                                  values: ok if either token can be placed
+#                                          dark if only a dark token can be placed
+#                                          light if only a light token can be placed
+#                                          end if no tokens can be placed
+
+# Happy Path Acceptance Tests
+    def test010_NominalLightDarkBlankBoardIntegrity(self):
+        self.setLight(1)
+        self.setDark(2)
+        self.setBlank(0)
+        self.setBoard([0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+        self.setIntegrity('6c3ec0129f5e128f48e2541bd6663a52a825c35f99b9a69d9593f2fc44b0bb4b')
+        expectedResult = {'status': 'ok'}
+        result = status._status(self.inputDictionary)
+        self.assertEqual(result, expectedResult)
+
+        

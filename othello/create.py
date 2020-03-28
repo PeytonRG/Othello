428d064b682518631b80e99177e765e820319aab
Peyton Gasink <peytongasink@att.net>
2020-03-28 16:45:16 -0500

    Refactor |

--- othello/create.py
+++ othello/create.py
@@ -4,17 +4,18 @@
     @author: Peyton Gasink
 '''
 import hashlib
+from othello import parmValidation
 
 def _create(inputDictionary):
     errorList = []
     
-    light = _validateLight(inputDictionary, errorList)
+    light = parmValidation._validateLight(inputDictionary, errorList)
     
-    dark = _validateDark(inputDictionary, errorList)
+    dark = parmValidation._validateDark(inputDictionary, errorList)
     
-    blank = _validateBlank(inputDictionary, errorList)
+    blank = parmValidation._validateBlank(inputDictionary, errorList)
         
-    lengthWidth = _validateSize(inputDictionary, errorList)
+    lengthWidth = parmValidation._validateSize(inputDictionary, errorList)
            
     if light == dark or light == blank or dark == blank:
         errorList.append("The values of light, dark, " 
@@ -39,93 +40,7 @@ def _create(inputDictionary):
         }
     return result
 
-def _validateLight(inputDictionary, errorList):
-    errorMessage = ("The value for light tokens must be an integer in the " + 
-                    "range [0, 9].")
-    try:
-        light = inputDictionary["light"]
-        if isinstance(light, float):
-            raise ValueError
-        else:
-            # cast the string from the HTTP request into int
-            # will raise ValueError upon failure
-            light = int(light)
-            
-        if light > 9 or light < 0:
-            errorList.append(errorMessage)
-            
-    except KeyError:
-        light = 1
-    except (ValueError, TypeError):
-        light = 1
-        errorList.append(errorMessage)
-    return light
 
-def _validateDark(inputDictionary, errorList):
-    errorMessage = ("The value for dark tokens must be an integer in the " + 
-                    "range [0, 9].")
-    try:
-        dark = inputDictionary["dark"]
-        if isinstance(dark, float):
-            raise ValueError
-        else:
-            # cast the string from the HTTP request into int
-            # will raise ValueError upon failure
-            dark = int(dark)
-            
-        if dark > 9 or dark < 0:
-            errorList.append(errorMessage)
-            
-    except KeyError:
-        dark = 2
-    except (ValueError, TypeError):
-        dark = 2
-        errorList.append(errorMessage)
-    return dark
-
-def _validateBlank(inputDictionary, errorList):
-    errorMessage = ("The value for blank spaces must be an integer in the " + 
-                    "range [0, 9].")
-    try:
-        blank = inputDictionary["blank"]
-        if isinstance(blank, float):
-            raise ValueError
-        else:
-            # cast the string from the HTTP request into int
-            # will raise ValueError upon failure
-            blank = int(blank)
-            
-        if blank > 9 or blank < 0:
-            errorList.append(errorMessage)
-            
-    except KeyError:
-        blank = 0
-    except (ValueError, TypeError):
-        blank = 0
-        errorList.append(errorMessage)
-    return blank
-
-def _validateSize(inputDictionary, errorList):
-    errorMessage = ("The value for board size must be an even integer in " + 
-                    "the range [6, 16].")
-    try:
-        lengthWidth = inputDictionary["size"]
-        if isinstance(lengthWidth, float):
-            raise ValueError
-        else:
-            # cast the string from the HTTP request into int
-            # will raise ValueError upon failure
-            lengthWidth = int(lengthWidth)
-            
-        if (lengthWidth % 2 != 0) or (lengthWidth > 16) or (lengthWidth < 6):
-            errorList.append(errorMessage)
-            
-    except KeyError:
-        lengthWidth = 8
-    except (ValueError, TypeError):
-        lengthWidth = 8
-        errorList.append(errorMessage)
-    return lengthWidth
 
 def _generateBoard(light, dark, blank, lengthWidth):
     boardSize = lengthWidth ** 2
---
+++ othello/parmValidation.py
@@ -0,0 +1,87 @@
+def _validateLight(inputDictionary, errorList):
+    errorMessage = ("The value for light tokens must be an integer in the " + 
+                    "range [0, 9].")
+    try:
+        light = inputDictionary["light"]
+        if isinstance(light, float):
+            raise ValueError
+        else:
+            # cast the string from the HTTP request into int
+            # will raise ValueError upon failure
+            light = int(light)
+            
+        if light > 9 or light < 0:
+            errorList.append(errorMessage)
+            
+    except KeyError:
+        light = 1
+    except (ValueError, TypeError):
+        light = 1
+        errorList.append(errorMessage)
+    return light
+
+def _validateDark(inputDictionary, errorList):
+    errorMessage = ("The value for dark tokens must be an integer in the " + 
+                    "range [0, 9].")
+    try:
+        dark = inputDictionary["dark"]
+        if isinstance(dark, float):
+            raise ValueError
+        else:
+            # cast the string from the HTTP request into int
+            # will raise ValueError upon failure
+            dark = int(dark)
+            
+        if dark > 9 or dark < 0:
+            errorList.append(errorMessage)
+            
+    except KeyError:
+        dark = 2
+    except (ValueError, TypeError):
+        dark = 2
+        errorList.append(errorMessage)
+    return dark
+
+def _validateBlank(inputDictionary, errorList):
+    errorMessage = ("The value for blank spaces must be an integer in the " + 
+                    "range [0, 9].")
+    try:
+        blank = inputDictionary["blank"]
+        if isinstance(blank, float):
+            raise ValueError
+        else:
+            # cast the string from the HTTP request into int
+            # will raise ValueError upon failure
+            blank = int(blank)
+            
+        if blank > 9 or blank < 0:
+            errorList.append(errorMessage)
+            
+    except KeyError:
+        blank = 0
+    except (ValueError, TypeError):
+        blank = 0
+        errorList.append(errorMessage)
+    return blank
+
+def _validateSize(inputDictionary, errorList):
+    errorMessage = ("The value for board size must be an even integer in " + 
+                    "the range [6, 16].")
+    try:
+        size = inputDictionary["size"]
+        if isinstance(size, float):
+            raise ValueError
+        else:
+            # cast the string from the HTTP request into int
+            # will raise ValueError upon failure
+            size = int(size)
+            
+        if (size % 2 != 0) or (size > 16) or (size < 6):
+            errorList.append(errorMessage)
+            
+    except KeyError:
+        size = 8
+    except (ValueError, TypeError):
+        size = 8
+        errorList.append(errorMessage)
+    return size

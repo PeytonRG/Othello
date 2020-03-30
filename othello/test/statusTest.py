60fb74074a5f87aeef3818d93b38044a5fd6e2ff
Peyton Gasink <peytongasink@att.net>
2020-03-30 16:01:49 -0500

    Red Light |

--- othello/test/statusTest.py
+++ othello/test/statusTest.py
@@ -302,6 +302,17 @@ class statusTest(TestCase):
                           + 'be an integer in the range [0, 9].'}
         result = status._status(self.inputDictionary)
         self.assertEqual(result, expectedResult)
+        
+    def test911_BelowBoundDarkNominalLightBlankBoardIntegrity(self):
+        self.setLight(5)
+        self.setDark(-1)
+        self.setBlank(1)
+        self.setBoard([1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,-1,1,1,1,1,-1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+        self.setIntegrity('301e0f00c1b83b65adc1d4fd5e87aaf7f594aa20842ab1df86a6be2e144367db')
+        expectedResult = {'status': 'error: The value for dark tokens must '
+                          + 'be an integer in the range [0, 9].'}
+        result = status._status(self.inputDictionary)
+        self.assertEqual(result, expectedResult)
 
 # Unit Tests
 

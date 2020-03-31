f73e695c501b32f8c19c2347596f57d7e15e091d
Peyton Gasink <peytongasink@att.net>
2020-03-30 19:43:28 -0500

    Green Light |

--- othello/test/statusTest.py
+++ othello/test/statusTest.py
@@ -82,7 +82,7 @@ class statusTest(TestCase):
         self.setDark(2)
         self.setBlank(3)
         self.setBoard([3,3,3,3,3,3,3,3,3,3,3,3,3,3,1,2,3,3,3,3,2,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3])
-        self.setIntegrity('integrity=f01977c17f801c43eeb13fb9f74a49bd0c761db3cdffe01510f47ddd23ab465a')
+        self.setIntegrity('f01977c17f801c43eeb13fb9f74a49bd0c761db3cdffe01510f47ddd23ab465a')
         expectedResult = {'status': 'ok'}
         result = status._status(self.inputDictionary)
         self.assertEqual(result, expectedResult)

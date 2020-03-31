bafc4287bef9578955f30f10e2cb5806590e68d4
Peyton Gasink <peytongasink@att.net>
2020-03-30 19:45:58 -0500

    Red Light |

--- othello/test/statusTest.py
+++ othello/test/statusTest.py
@@ -429,12 +429,22 @@ class statusTest(TestCase):
         result = status._status(self.inputDictionary)
         self.assertEqual(result, expectedResult)
         
-    def test940_LongIntegrityNominalLightDarkBlankBoard(self):
+    def test941_LongIntegrityNominalLightDarkBlankBoard(self):
         self.setLight(1)
         self.setDark(2)
         self.setBlank(3)
         self.setBoard([3,3,3,3,3,3,3,3,3,3,3,3,3,3,1,2,3,3,3,3,2,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3])
         self.setIntegrity('f01977c17f801c43eeb13fb9f74a49bd0c761db3cdffe01510f47ddd23ab465a00')
+        expectedResult = {'status': 'error: The integrity string must be 64-character sha-256 hash hexdigest.'}
+        result = status._status(self.inputDictionary)
+        self.assertEqual(result, expectedResult)
+        
+    def test942_NonHexIntegrityNominalLightDarkBlankBoard(self):
+        self.setLight(1)
+        self.setDark(2)
+        self.setBlank(3)
+        self.setBoard([3,3,3,3,3,3,3,3,3,3,3,3,3,3,1,2,3,3,3,3,2,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3])
+        self.setIntegrity('f01977c17f801c43eeb13fb9f74a49bd0c761db3cdffe01510f47ddd23ab465$')
         expectedResult = {'status': 'error: The integrity string must be 64-character sha-256 hash hexdigest.'}
         result = status._status(self.inputDictionary)
         self.assertEqual(result, expectedResult)

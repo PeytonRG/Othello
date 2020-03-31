17cd22f26363ff815b368710a0daf322be112a3b
Peyton Gasink <peytongasink@att.net>
2020-03-31 18:37:32 -0500

    Refactor |

--- othello/status.py
+++ othello/status.py
@@ -72,7 +72,7 @@ def _calculateMoveCount(currentToken, position, board, light, dark, blank):
         "rowBounding": _enforceRowBoundingLeft
     }
     
-    possibleMoves += _scanOutwardFromToken(position, elementsInRow, currentToken, oppositeToken,
+    possibleMoves += _scanInOneDimension(position, elementsInRow, currentToken, oppositeToken,
                                      blank, board, directionFunctions)
     
     # Direction: Horizontal Right
@@ -82,7 +82,7 @@ def _calculateMoveCount(currentToken, position, board, light, dark, blank):
         "rowBounding": _enforceRowBoundingRight
     }
     
-    possibleMoves += _scanOutwardFromToken(position, elementsInRow, currentToken, oppositeToken,
+    possibleMoves += _scanInOneDimension(position, elementsInRow, currentToken, oppositeToken,
                                      blank, board, directionFunctions)
     
     # Direction: Vertical Up
@@ -92,7 +92,7 @@ def _calculateMoveCount(currentToken, position, board, light, dark, blank):
         "rowBounding": _enforceRowBoundingAbove
     }
     
-    possibleMoves += _scanOutwardFromToken(position, elementsInRow, currentToken, oppositeToken,
+    possibleMoves += _scanInOneDimension(position, elementsInRow, currentToken, oppositeToken,
                                      blank, board, directionFunctions)
         
     # Direction: Vertical Down
@@ -102,10 +102,21 @@ def _calculateMoveCount(currentToken, position, board, light, dark, blank):
         "rowBounding": _enforceRowBoundingBelow
     }
     
-    possibleMoves += _scanOutwardFromToken(position, elementsInRow, currentToken, oppositeToken,
+    possibleMoves += _scanInOneDimension(position, elementsInRow, currentToken, oppositeToken,
                                      blank, board, directionFunctions)
     
     # Direction: Diagonal Up, Left
+    directionFunctions = {
+        "offset": _getOffsetAboveLeft,
+        "loopCondition": _loopConditionAbove,
+        "rowBounding": _enforceRowBoundingAboveLeft,
+        "wrapAround": _enforceWrapAroundAboveLeft,
+        "verticalNeighbor": _getOffsetAbove
+    }
+    
+    possibleMoves += _scanInTwoDimensions(position, elementsInRow, currentToken, oppositeToken,
+                                     blank, board, directionFunctions)
+    
     indexOfAboveLeftAdjacent = position - elementsInRow - 1
     # Only enter the loop if there is a row above the current row
     while indexOfAboveLeftAdjacent > 0:
@@ -215,7 +226,7 @@ def _calculateMoveCount(currentToken, position, board, light, dark, blank):
 
     return possibleMoves
 
-def _scanOutwardFromToken(position, elementsInRow, currentToken, oppositeToken, blank, board, directionFunctions):
+def _scanInOneDimension(position, elementsInRow, currentToken, oppositeToken, blank, board, directionFunctions):
     possibleMoves = 0
     
     # get the row of this token to compare with the tokens that surround it
@@ -240,6 +251,39 @@ def _scanOutwardFromToken(position, elementsInRow, currentToken, oppositeToken, 
         
     return possibleMoves
 
+def _scanInTwoDimensions(position, elementsInRow, currentToken, oppositeToken, blank, board, directionFunctions):
+    possibleMoves = 0
+    
+    # get the row of this token to compare with the tokens that surround it
+    # by dividing the first position in the row by the number of elements in a row
+    firstIndexInRow = position - position % elementsInRow
+    
+    indexOfNeighbor = directionFunctions["offset"](elementsInRow, position)
+    # Only enter the loop if there is a row above the current row
+    while directionFunctions["loopCondition"](board, elementsInRow, firstIndexInRow, indexOfNeighbor):
+        neighborValue = board[indexOfNeighbor]
+        indexOfVerticalNeighbor = directionFunctions["verticalNeighbor"](elementsInRow, position)
+        lookahead = directionFunctions["offset"](elementsInRow, indexOfNeighbor)
+        # Ensure the next space to check is also on the board
+        if directionFunctions["rowBounding"](board, elementsInRow, indexOfVerticalNeighbor, lookahead):
+            
+            firstIndexInNeighboringRow = indexOfVerticalNeighbor - indexOfVerticalNeighbor % elementsInRow
+            
+            # In addition to the usual checks for token colors, ensure no wrapping around
+            # to the other side of the board
+            if (directionFunctions["wrapAround"](elementsInRow, firstIndexInNeighboringRow, indexOfNeighbor) 
+                and neighborValue == oppositeToken and board[lookahead] == blank):
+                
+                possibleMoves += 1
+                break
+            elif neighborValue == blank or neighborValue == currentToken:
+                break
+            else:
+                indexOfNeighbor = directionFunctions["offset"](elementsInRow, indexOfNeighbor)
+        else: 
+            break
+    
+    return possibleMoves
 
 def _loopConditionAbove(board, elementsInRow, firstIndexInRow, indexOfAboveNeighbor):
     return indexOfAboveNeighbor > 0
@@ -259,6 +303,9 @@ def _loopConditionRight(board, elementsInRow, firstIndexInRow, indexOfHorizontal
 def _enforceRowBoundingAbove(board, elementsInRow, firstIndexInRow, lookahead):
     return lookahead >= 0
 
+def _enforceRowBoundingAboveLeft(board, elementsInRow, indexOfVerticalNeighbor, lookahead):
+    return indexOfVerticalNeighbor >= 0 and lookahead >= 0
+
 def _enforceRowBoundingBelow(board, elementsInRow, firstIndexInRow, lookahead):
     return lookahead <= len(board) - 1
            
@@ -269,8 +316,14 @@ def _enforceRowBoundingRight(board, elementsInRow, firstIndexInRow, lookahead):
     lastIndexInRow = firstIndexInRow + elementsInRow - 1
     return lookahead <= lastIndexInRow
 
+def _enforceWrapAroundAboveLeft(elementsInRow, firstIndexInNeighboringRow, indexOfNeighbor):
+    return indexOfNeighbor >= firstIndexInNeighboringRow
+
 def _getOffsetAbove(elementsInRow, position):
     return position - elementsInRow
+
+def _getOffsetAboveLeft(elementsInRow, position):
+    return position - elementsInRow - 1
 
 def _getOffsetBelow(elementsInRow, position):
     return position + elementsInRow
